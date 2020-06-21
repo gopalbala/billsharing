9b8baa7e8d398985ad4cb3dc7d34c6742715b33d
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-06-21 16:04:13 +0530

    logic to add user contribution

--- src/main/java/com/gb/billsharing/service/UserService.java
+++ src/main/java/com/gb/billsharing/service/UserService.java
@@ -1,6 +1,10 @@
 package com.gb.billsharing.service;
 
-import com.gb.billsharing.model.User;
+import com.gb.billsharing.exceptions.ContributionExceededException;
+import com.gb.billsharing.exceptions.ExpenseSettledException;
+import com.gb.billsharing.exceptions.InvalidExpenseState;
+import com.gb.billsharing.model.*;
+import com.gb.billsharing.repository.ExpenseRepository;
 import com.gb.billsharing.repository.UserRepository;
 
 import java.util.UUID;
@@ -14,5 +18,24 @@ public class UserService {
                 .build();
         UserRepository.userHashMap.putIfAbsent(emailId, user);
         return user;
+    }
+
+    public void contributeToExpense(String expenseId, String emailId, Contribution contribution)
+            throws InvalidExpenseState, ExpenseSettledException, ContributionExceededException {
+        Expense expense = ExpenseRepository.expenseMap.get(expenseId);
+        ExpenseGroup expenseGroup = expense.getExpenseGroup();
+        if (expense.getExpenseStatus() == ExpenseStatus.CREATED) {
+            throw new InvalidExpenseState("Invalid expense State");
+        }
+        if (expense.getExpenseStatus() == ExpenseStatus.SETTLED) {
+            throw new ExpenseSettledException("Expense is already settled.");
+        }
+        UserShare userShare = expenseGroup.getUserContributions().get(emailId);
+        if (contribution.getContributionValue() > userShare.getShare()) {
+            throw new ContributionExceededException(
+                    String.format("User %s contribution %d exceeded the share %d",
+                            emailId, contribution.getContributionValue(), userShare.getShare()));
+        }
+        userShare.getContributions().add(contribution);
     }
 }
