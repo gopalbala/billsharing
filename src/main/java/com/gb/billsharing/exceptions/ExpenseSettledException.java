29bb2404cf6bbc476f15848b563dc350c3d0a7c3
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-06-21 15:53:56 +0530

    Added expense settled exception and invalid expense state exception

---
+++ src/main/java/com/gb/billsharing/exceptions/ExpenseSettledException.java
@@ -0,0 +1,7 @@
+package com.gb.billsharing.exceptions;
+
+public class ExpenseSettledException extends Exception {
+    public ExpenseSettledException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/gb/billsharing/exceptions/InvalidExpenseState.java
@@ -0,0 +1,13 @@
+package com.gb.billsharing.exceptions;
+
+public class InvalidExpenseState extends Exception {
+
+    public InvalidExpenseState(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
