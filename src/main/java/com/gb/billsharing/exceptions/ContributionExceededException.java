8bcc3617a82dc2131291445a3b158f182293839c
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-06-21 16:02:44 +0530

    Added user contribution exceeded exception

---
+++ src/main/java/com/gb/billsharing/exceptions/ContributionExceededException.java
@@ -0,0 +1,7 @@
+package com.gb.billsharing.exceptions;
+
+public class ContributionExceededException extends Exception {
+    public ContributionExceededException(String message) {
+        super(message);
+    }
+}
